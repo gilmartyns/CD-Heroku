31492a029a07a50cd1a90d0f64363e2048f61164
gilmartyns <89709782+gilmartyns@users.noreply.github.com>
2021-10-04 14:43:21 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Não esquece que te amo Dioli"
+    return "Givanildo Martins Pereira"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
